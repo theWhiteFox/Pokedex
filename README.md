cb9bb695c7f62514a7e3837f83dab84b00e6d833
Stíofán ♔ Ó Conchubhair <stevo.joc@gmail.com>
2022-09-09 16:04:43 +0100

    graphQL set up :wrench:

---
+++ README.md
@@ -0,0 +1,30 @@
+# theWhiteFox Vite + React + GraphQL boilerplate
+
+* `node -v` v16.17.0
+* `yarn -v` 1.15.2
+
+- `yarn create vite`
+ -- select react-ts
+- `yarn`
+- `yarn add graphql graphql-tag`
+- `yarn add @apollo/react-hooks`
+- `yarn add react-jss`
+- `yarn add react-router-dom`
+- `yarn add @mui/material @emotion/react @emotion/styled`
+- `yarn add @mui/icons-material`
+- `yarn add @types/react @types/react-dom`
+- `yarn add -D jest @types/jest`
+- `yarn add -D eslint`
+- `npx eslint --init`
+- `npx install-peerdeps --dev eslint-config-airbnb`
+- ` yarn add -D eslint-config-airbnb-typescript`
+- ` yarn add -D prettier eslint-config-prettier eslint-plugin-prettier`
+- `yarn add -D vitest`
+- `yarn add --dev @testing-library/react`
+- `yarn add --dev @testing-library/jest-dom`
+- `yarn dev`
+
+## Ref
+- [Medium Quick Jest Setup Vite React TS](https://codingwithmanny.medium.com/quick-jest-setup-with-vitejs-react-typescript-82f325e4323f)
+- [Markus Oberlehner using-testing-library-jest-dom-with-vitest](https://markus.oberlehner.net/blog/using-testing-library-jest-dom-with-vitest/)
+- [https://testing-library.com/docs/queries/about](https://testing-library.com/docs/queries/about)
---
+++ src/app/App.tsx
@@ -0,0 +1,58 @@
+import { createUseStyles } from 'react-jss';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { ApolloProvider } from '@apollo/client';
+import { client } from './client';
+
+function App() {
+  const classes = useStyles();
+
+  return (
+    <ApolloProvider client={client}>
+      <LayoutProvider>
+        <div className={classes.root}>
+          <BrowserRouter>
+            <Nav />
+            <div className={classes.content}>
+              <div className={classes.scrollableArea}>
+                <Routes location={location}>
+                  <Route path="/" element={<Home />} />
+                  <Route path="/pokemon" element={<ListPage />} />
+                  <Route path="/pokemon/:pokeName" element={<Modal />} />
+                </Routes>
+              </div>
+            </div>
+          </BrowserRouter>
+        </div>
+      </LayoutProvider>
+    </ApolloProvider>
+  );
+}
+
+const useStyles = createUseStyles(
+  {
+    root: {
+      background: '#171E2b',
+      minHeight: '100vh',
+      minWidth: '100vw',
+      height: '100%',
+      width: '100%',
+      display: 'flex',
+    },
+    content: {
+      flex: '1',
+      overflow: 'hidden',
+      position: 'relative',
+    },
+    scrollableArea: {
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      right: 0,
+      bottom: 0,
+      overflow: 'auto',
+    },
+  },
+  { name: 'App' }
+);
+
+export default App;
